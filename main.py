6c61560f1ceb907aff41d3fe7d36d7bd8becf06c
Dalilah Ali <dali11@hawkmail.hccfl.edu>
2024-09-27 20:31:53 -0400

    Initialized variables and added IF statements.

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ This program calculates prices for custom house signs.
 # Number of characters.
 # Color of characters.
 # Type of wood.
-charge=0.00
+charge=35.00
 numChars=8
 color="gold"
 woodType="oak"
 
+numChars=int(input("Enter number of characters: "))
+woodType=str(input("Enter wood type: "))
+color=str(input("Enter color: "))
+
 
 # Write assignment and if statements here as appropriate.
+if numChars > 5:
+    charge = charge + (numChars-5)*4
 
+if woodType=="oak":
+    charge = charge + 20
+
+if color == "gold":
+    charge = charge + 15
 # Output Charge for this sign.
 print("The charge for this sign is $" + str(charge))
 
