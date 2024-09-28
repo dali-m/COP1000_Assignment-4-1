e2aaab0a280d1ea5902186ebdcd3d80f7b4ce4f7
Dalilah Ali <dali11@hawkmail.hccfl.edu>
2024-09-27 20:11:34 -0400

    updates

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ This program calculates prices for custom house signs.
 # Number of characters.
 # Color of characters.
 # Type of wood.
+charge=0.00
+numChars=8
+color="gold"
+woodType="oak"
+
 
 # Write assignment and if statements here as appropriate.
 
